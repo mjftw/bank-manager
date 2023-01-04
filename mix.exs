380f489373d01415e231a8a650eee472618c7d19
Merlin Webster <merlin.webster@multiverse.io>
2023-01-04 19:54:03 +0000

    Switch to Elixir 1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Bank.MixProject do
     [
       app: :bank,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
