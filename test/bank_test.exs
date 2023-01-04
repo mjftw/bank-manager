2d98b8a16376c0f40f96a043669cee5477f4cbaa
Merlin Webster <merlin.webster@multiverse.io>
2023-01-04 20:23:14 +0000

    Use aliases in tests

--- test/bank_test.exs
+++ test/bank_test.exs
@@ -1,25 +1,28 @@
 defmodule BankTest do
   use ExUnit.Case
 
+  alias Bank.Account
+  alias Bank.Transaction
+
   setup do
     %{now: DateTime.now("Etc/UTC")}
   end
 
   describe "balance/1" do
     test "should give correct balance when no transactions" do
-      account = %Bank.Account{account_num: 123, initial_balance: 100}
+      account = %Account{account_num: 123, initial_balance: 100}
 
       assert Bank.Core.balance(account) == 100
     end
 
     test "should give correct balance with transactions", %{now: now} do
-      account = %Bank.Account{
+      account = %Account{
         account_num: 123,
         initial_balance: 100,
         transactions: [
-          %Bank.Transaction{amount: 10, account_num: 321, datetime: now},
-          %Bank.Transaction{amount: 75, account_num: 321, datetime: now},
-          %Bank.Transaction{amount: -15, account_num: 321, datetime: now}
+          %Transaction{amount: 10, account_num: 321, datetime: now},
+          %Transaction{amount: 75, account_num: 321, datetime: now},
+          %Transaction{amount: -15, account_num: 321, datetime: now}
         ]
       }
 
@@ -29,8 +32,8 @@ defmodule BankTest do
 
   describe "transfer/3" do
     test "should add complementary transactions to both accounts" do
-      from = %Bank.Account{account_num: 321, initial_balance: 100}
-      to = %Bank.Account{account_num: 123, initial_balance: 100}
+      from = %Account{account_num: 321, initial_balance: 100}
+      to = %Account{account_num: 123, initial_balance: 100}
 
       {:ok, {new_from, new_to}} = Bank.Core.transfer(from, to, 10)
       [from_transaction | _] = new_from.transactions
@@ -40,8 +43,8 @@ defmodule BankTest do
     end
 
     test "should give error when from account balance too low" do
-      from = %Bank.Account{account_num: 321, initial_balance: 100}
-      to = %Bank.Account{account_num: 123, initial_balance: 100}
+      from = %Account{account_num: 321, initial_balance: 100}
+      to = %Account{account_num: 123, initial_balance: 100}
 
       {:error, _} = Bank.Core.transfer(from, to, 101)
     end
